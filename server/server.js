17007475b066e03ecebb807fe7a3b67063a42afd
ryanyen2 <ryanyen2-c@my.cityu.edu.hk>
2021-06-15 21:01:09 +0800

    add static

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "../client/dist")))
+app.use(express.static(path.join(__dirname, "../client/dist")))
 // app.use('*', express.static(path.join(__dirname, "../client/dist")));
 
 const routes = require('./routes/index')
